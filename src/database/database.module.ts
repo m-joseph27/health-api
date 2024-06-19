c81447b1af4e56a8492d21058757db0c72bfc327
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-06-19 20:36:52 +0700

    add dotenv configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/healthAPI')],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI)],
 })
 export class DatabaseModule {}
